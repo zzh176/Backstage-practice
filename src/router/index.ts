43a049dacc4fa50e09dd9f93daf275f965697121
zzh176 <1761890083@qq.com>
2020-09-10 08:54:20 +0800

    项目初始化

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
+import Header from "@/views/header/index.vue";
+import Nav from "@/views/nav/index.vue";
 
 Vue.use(VueRouter)
 
@@ -10,14 +12,6 @@ Vue.use(VueRouter)
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
+<!--  -->
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class>
+    <div class="header">
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+      >
+        <el-menu-item index="1" class="el-icon-user-solid">用户</el-menu-item>
+        <el-menu-item index="1" class="nav-icon.teach">教学</el-menu-item>
+        <el-menu-item index="1">题库</el-menu-item>
+        <el-menu-item index="1">运营</el-menu-item>
+        <el-menu-item index="1">营销</el-menu-item>
+        <el-menu-item index="1">商品</el-menu-item>
+        <el-menu-item index="1">财务</el-menu-item>
+        <el-menu-item index="1">统计</el-menu-item>
+        <el-menu-item index="1">系统</el-menu-item>
+      </el-menu>
+    </div>
+    <div class="nav"></div>
+    <div class="main"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  //方法集合
+  methods: {},
+  created() {},
+  mounted() {},
+};
 </script>
+<style lang='scss' scoped>
+* {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+.header {
+  width: 100%;
+  height: 50px;
+  border: 1px solid #000;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+}
+</style>
